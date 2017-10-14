86d6e660212dd33cef58711b3c06041ebf4b4dd2
ChinoLee <you@example.com>
2017-10-14 22:48:03 +0800

    ssssss1

--- app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.chino_lee.mobileguard", appContext.getPackageName());
+        assertEquals("cn.edu.gdmec.android.mobileguard", appContext.getPackageName());
     }
 }
