0b0aae4ecae796a0d0fe3783b6ee76cef89dc46e
ChinoLee <760349415@qq.com>
2017-11-20 22:51:57 +0800

    m6清理   10.5

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/CleanCacheActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache;
+
+import android.content.DialogInterface;
+
+/**
+ * Created by Chino-Lee on 2017/11/20.
+ */
+
+public class CleanCacheActivity implements DialogInterface.OnClickListener {
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
+    }
+}
