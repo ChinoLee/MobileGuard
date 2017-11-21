19ae8bb65f0461468533cc639590cc81650706b0
ChinoLee <760349415@qq.com>
2017-11-21 20:00:43 +0800

    m6清理   10.12

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
@@ -1,8 +1,14 @@
 package cn.edu.gdmec.android.mobileguard.m6cleancache.entity;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by Chino-Lee on 2017/11/20.
  */
 
 public class CacheInfo {
+    public String packagename;
+    public long cacheSize;
+    public Drawable appIcon;
+    public String appName;
 }
