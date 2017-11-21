ff64a61eb6ddd4857bbde36c2f86e0225c7720ae
07160820ljl <1114316513@qq.com>
2017-11-21 14:58:34 +0800

    021

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
@@ -11,7 +11,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 /**
- * Created by student on 17/10/17.
+ * Created by student on 17/10/17.132132
  */
 
 public class App extends Application{
