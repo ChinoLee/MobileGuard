7052d11427d06380760a53fcf89749c71a8b658f
ChinoLee <760349415@qq.com>
2017-11-23 16:54:26 +0800

    11.21课堂练习测试

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,51 +1,47 @@
 package cn.edu.gdmec.android.mobileguard;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
 
-import java.io.File;
-
+import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
+
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);}
+        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         mVersion = MyUtils.getVersion(getApplicationContext());
         mTvVersion=(TextView)findViewById(R.id.tv_splash_version);
         mTvVersion.setText("版本号"+mVersion);
-        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this);
+        VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
+            @Override
+            public void afterDownload(String filename) {
+                MyUtils.installApk(SplashActivity.this,filename);
+            }
+        };
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback, HomeActivity.class);
         new Thread(){
             @Override
             public void run(){
                 super.run();
-                versionUpdateUtils.getCloudVersion();
+                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
             }
         }.start();
-public static void installApk(Activity activity,String apkFile){
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addCategory(Intent.CATEGORY_DEFAULT);
-        intent.setDataAndType(Uri.fromFile(
-                new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)),
-                "application/Vnd.android.package-archive");
-        activity.startActivityForResult(intent,0);
-
-
+       /* startActivity(new Intent(this, HomeActivity.class));
+        finish();*/
     }
-    }
+
+}
 
 
 
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -1,8 +1,16 @@
 package cn.edu.gdmec.android.mobileguard.m1home.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
+
+import cn.edu.gdmec.android.mobileguard.SplashActivity;
 
 /**
  * Created by 10255 on 2017/9/21.
@@ -19,5 +27,13 @@ public class MyUtils {
             e.printStackTrace();
             return "";
         }
+    }
+
+
+    public static void installApk(Activity activity, String apkFile) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)),"application/vnd.android.pack-archive");
+        activity.startActivityForResult(intent,0);
     }
 }
